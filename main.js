0fd73242ed72e870548b1e76b93eed2ae36bfbfb
Kenneth Gargan <44318081+Kenneth-G@users.noreply.github.com>
2019-03-13 21:25:04 +0000

    changes to animation

--- main.js
+++ main.js
@@ -5,41 +5,53 @@ let newPage;
 
 
 function navClick(event){
-    
+
     /*
         On click we check to see if a new page was clicked, if so we change the content to the clicked page.
     */
 
     let newNavClick = event.target.className.toString().split(' ')[2];
-        //Check to see if a different page was clicked.
-        if(currentPage !== newNavClick ){
-            setNewPage(newNavClick);
-            contentChange();
-            setCurrentPage(getNewPage());
-        }
+    //Check to see if a different page was clicked.
+    if(currentPage !== newNavClick ){
+        setNewPage(newNavClick);
+        contentChange();
+        setCurrentPage(getNewPage());
+    }
 }
 
 //this will change the content to the right page. 
 function contentChange(){
-    let targetCurrentElement = document.getElementById(getCurrentPage());
-    let targetNewElement = document.getElementById(getNewPage());
+    let targetCurrentElementParent = document.getElementById(getCurrentPage());
+    let targetCurrentElementChild = document.getElementById(getCurrentPage()+'-wrapper');
+
+    let targetNewElementParent = document.getElementById(getNewPage());
+    let targetNewElementChild = document.getElementById(getNewPage()+'-wrapper');
+
 
     //Fade out current section element
-    targetCurrentElement.classList.add('fadeOut');
+    targetCurrentElementChild.classList.add('fadeOut');
 
-    //Wait until the animation is done before playing the FadeIn animation on the new requested page
+    //set the visibility 
     window.setTimeout(function(){
-        targetCurrentElement.style.display = 'none';
-        targetCurrentElement.style.visibility = 'hidden';
-        targetCurrentElement.classList.remove('fadeOut');
-        
-        targetNewElement.style.visibility = 'visible';
-        targetNewElement.style.display = 'flex';
-        targetNewElement.classList.add('fadeIn');
+        targetCurrentElementParent.style.visibility = 'hidden';
+    },900);
+
+    //Wait until the animation is done before we fadeIn and remove classes
+    window.setTimeout(function(){
+        targetCurrentElementParent.style.display = 'none';
+
+
+        targetNewElementParent.style.visibility = 'visible';
+        targetNewElementParent.style.display = 'flex';
+        targetNewElementChild.classList.add('fadeIn');
+
 
         //Wait until the fadeIn animation is done before removing it
         window.setTimeout(function(){
-            targetNewElement.classList.remove('fadeIn');
+            targetCurrentElementChild.classList.remove('fadeOut');
+            targetNewElementChild.classList.remove('fadeIn');
+
+
         },1000);
     },1000);
 }
