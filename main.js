1868a3c3d55076d913560048d251b360c4462935
Kenneth Gargan <44318081+Kenneth-G@users.noreply.github.com>
2019-03-14 10:52:33 +0000

    fixed flickering bug on chrome

--- main.js
+++ main.js
@@ -5,32 +5,32 @@ let newPage;
 
 
 function navClick(event){
-
+    
     /*
         On click we check to see if a new page was clicked, if so we change the content to the clicked page.
     */
 
     let newNavClick = event.target.className.toString().split(' ')[2];
-    //Check to see if a different page was clicked.
-    if(currentPage !== newNavClick ){
-        setNewPage(newNavClick);
-        contentChange();
-        setCurrentPage(getNewPage());
-    }
+        //Check to see if a different page was clicked.
+        if(currentPage !== newNavClick ){
+            setNewPage(newNavClick);
+            contentChange();
+            setCurrentPage(getNewPage());
+        }
 }
 
 //this will change the content to the right page. 
 function contentChange(){
     let targetCurrentElementParent = document.getElementById(getCurrentPage());
     let targetCurrentElementChild = document.getElementById(getCurrentPage()+'-wrapper');
-
+    
     let targetNewElementParent = document.getElementById(getNewPage());
     let targetNewElementChild = document.getElementById(getNewPage()+'-wrapper');
-
+    
 
     //Fade out current section element
     targetCurrentElementChild.classList.add('fadeOut');
-
+    
     //set the visibility 
     window.setTimeout(function(){
         targetCurrentElementParent.style.visibility = 'hidden';
@@ -38,20 +38,17 @@ function contentChange(){
 
     //Wait until the animation is done before we fadeIn and remove classes
     window.setTimeout(function(){
+        targetCurrentElementChild.classList.remove('fadeOut');
         targetCurrentElementParent.style.display = 'none';
-
-
+        
         targetNewElementParent.style.visibility = 'visible';
         targetNewElementParent.style.display = 'flex';
         targetNewElementChild.classList.add('fadeIn');
-
+        
 
         //Wait until the fadeIn animation is done before removing it
         window.setTimeout(function(){
-            targetCurrentElementChild.classList.remove('fadeOut');
             targetNewElementChild.classList.remove('fadeIn');
-
-
         },1000);
     },1000);
 }
